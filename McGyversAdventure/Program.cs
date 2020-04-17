374332477ad290de28770354fa51eb66c31cb094
Jens.Agersgaard <alex.norgaard@gmail.com>
2020-04-17 10:57:00 +0200

    Add project files.

---
+++ McGyversAdventure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace McGyversAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
